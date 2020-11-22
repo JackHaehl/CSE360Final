001d66371d14d9e3cea08a78f4a93c483000f69d
Ryder <ryder.loves.basketball@gmail.com>
2020-11-21 22:04:41 -0700

    JACK LOOK AT THIS :)

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
         //ryder made a change here :)
+        int i = 5;
+        while (i < 100){
+            System.out.println(i);
+            i++;
+        }
     }
 }
