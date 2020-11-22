13393e50cc012a0b480ac3c29c964d090c24979b
Jack Haehl <jackeliash@gmail.com>
2020-11-21 22:06:48 -0700

    Test commit again
    Does it work Ryder?

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
         //ryder made a change here :)
+        /**
+         * This does nothing useful
+         * Should be a for loop
+         * @return shame
+         */
         int i = 5;
         while (i < 100){
             System.out.println(i);
