392c8eb914ff3beff8c1371b98d63c229d0bd09d
Jack Haehl <jackeliash@gmail.com>
2020-11-26 01:13:49 -0700

    Clarified JMenu and Fixed IntelliJ nonsense

--- Java/src/WorldTest.java
+++ Java/src/WorldTest.java
@@ -4,13 +4,17 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class WorldTest {
     public static void main(String[] args){
 
+        //Initialize Frame
         JFrame f = new JFrame("Menu Demo");
+
 
         JMenuBar mb = new JMenuBar();
 
+        //Initialize buttons on menu
         JMenu x = new JMenu("Menu");
         JMenu y = new JMenu("Students");
 
+        //Initialize sub-buttons that drop down from menu buttons
         JMenuItem m1 = new JMenuItem("MenuItem1");
         JMenuItem m2 = new JMenuItem("MenuItem2");
         JMenuItem m3 = new JMenuItem("MenuItem3");
@@ -27,6 +31,7 @@ public class WorldTest {
 
         mb.add(y);
 
+        //Sets the menu bar of the frame to our chosen menu bar.
         f.setJMenuBar(mb);
 
         f.setSize(500,500);
